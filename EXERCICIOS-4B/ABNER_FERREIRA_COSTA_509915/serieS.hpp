9e8021fe8ce89b894283ed7ef0e8d0fa5b8217a5
Abner Ferreira <95752814+Abner432@users.noreply.github.com>
2022-07-03 11:08:29 -0300

    Delete EXERCICIOS-4B/ABNER_FERREIRA_COSTA_509915 directory

--- EXERCICIOS-4B/ABNER_FERREIRA_COSTA_509915/serieS.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef SERIES_HPP_INCLUDED //guarda
-#define SERIES_HPP_INCLUDED //guarda
-
-double serieS(int n); //protótipo da função
-
-#endif // SERIES_HPP_INCLUDED
--- EXERCICIOS-4B/ABNER_FERREIRA_COSTA_509915/tetranacci.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef TETRANACCI_HPP_INCLUDED//guarda
-#define TETRANACCI_HPP_INCLUDED//guarda
-
-int tetra(int n); //prototipa a função
-
-#endif // TETRANACCI_HPP_INCLUDED
